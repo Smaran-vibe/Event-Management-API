8febc96b7e7eb1f6fd1943fef356ecac6e4d0711
Smaran <smaranaryal60@gmail.com>
2026-07-24 21:38:08 +0545

    updated server.js

--- src/server.js
+++ src/server.js
@@ -16,8 +16,8 @@ const startServer = async () => {
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
-    } catch (error) {
-        console.error("Failed to start server:", error.message);
+    } catch (err) {
+        console.error("Failed to start server:", err.message);
     }
 };
 
