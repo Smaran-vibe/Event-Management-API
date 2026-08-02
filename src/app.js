8f7997c014ec643f7d72437c7f75d385cee2400a
Smaran <smaranaryal60@gmail.com>
2026-08-02 20:38:53 +0545

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const eventRoutes = require("./routes/event.routes");
+const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const registrationRoutes = require("./routes/registration.routes");
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/events", eventRoutes);
+
+app.use("/api/auth", authRoutes);
 
 app.use("/api/users", userRoutes);
 
